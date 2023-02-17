f395665f8a158137d091d9ed7f73545904613030
MJSneha <94105873+MJSneha@users.noreply.github.com>
2023-02-17 18:35:15 +0530

    Add files via upload

---
+++ PES1UG20CS594-1.cpp
@@ -0,0 +1,9 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Jenkins\n";
+  cout << "PES1UG20CS594 is successfully built and run\n";
+  return 0;
+}
