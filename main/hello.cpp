9d755712451e406a4dba16395b23caddd687ccd2
MJSneha <94105873+MJSneha@users.noreply.github.com>
2023-02-17 17:57:20 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "PES1UG20CS594 is successfully built and run\n";
+  cout << "PES1UG20CS594 is successfully built and run 2nd time\n";
   return 0;
 }
